366a95f7da8d555eaa383f57294a9660264deaab
Felicity Brammer <felicitybrammer@gmail.com>
2023-01-15 15:40:43 -0800

    styling and spacing blank grid

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var tasks = {};
+
+// create a task by clicking on the 
+//var createTask = function (taskTime
+
+
+// display current day at top of calendar <p id="currentDay" class="lead"></p> in header
+
+// load planner page with time blocks and any saved tasks <div class="container">
+
+
+
+// function to save tasks (on click. save in local storage)
+// .saveBtn
+//function to audit tasks
+// .future .present .past
+
+
+
+
+
+
+//load page
